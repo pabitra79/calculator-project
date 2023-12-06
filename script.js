88f0ca1253b7bc9ffed1bc291ba29c5e396a20b7
barik.pabitra <barikpabitra79@gmail.com>
2023-12-06 22:34:27 +0530

    index.html_calcualtor

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Starting server");
+let inputBox = document.getElementById('inputbox');
+function appendNumber(number) {
+    inputBox.value += number;
+}
+
+function appendOperator(operator) {
+    inputBox.value += operator;
+}
+
+function clearInput() {
+    inputBox.value = '';
+}
+
+function deleteLast() {
+    inputBox.value = inputBox.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        inputBox.value = eval(inputBox.value);
+    } catch (error) {
+        inputBox.value = 'Error';
+    }
+}
+
